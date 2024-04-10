60eb9250dfe02d89282c91a069fc63536b13c86f
MadScientist-24 <108148261+MadScientist-24@users.noreply.github.com>
2024-04-10 22:04:57 +0100

    ~hello world

---
+++ src/app/components/nav.tsx
@@ -0,0 +1,4 @@
+export default function NAV() {
+    return ( <h1>hello world hh</h1>);
+  }
+  
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import NAV from "./components/nav";
+
+export default function Home() {
+  return (
+    <NAV></NAV>
+   );
+}
